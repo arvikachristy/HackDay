d9e3b71a0280467ca9b3328edcc9a5f7530e1983
Anastasia Arvika Christy <anastasia.arvika@gmail.com>
2015-10-10 16:04:05 +0100

    Vika Addition

---
+++ localHackDay/TaskViewController.h
@@ -0,0 +1,13 @@
+//
+//  TaskViewController.h
+//  localHackDay
+//
+//  Created by Burns on 10/10/2015.
+//  Copyright © 2015 team. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TaskViewController : UIViewController
+
+@end
